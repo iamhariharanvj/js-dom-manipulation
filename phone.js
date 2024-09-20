f73e7e122cca7d0520e31730793becf2a6717e18
iamhariharanvj <hariharanvj2003@gmail.com>
2024-09-20 10:28:45 +0530

    Prototype: Mobile phone task - 20/09/24

--- phone.js
+++ phone.js
@@ -1,55 +1,80 @@
-const Phone = {
-    call: function(number) {
-        alert("Calling " + number);
-    },
-    sendMessage: function(message) {
-        alert("Sending message : " + message);
-    },
-    browse: function(url) {
-        alert("Browsing to " + url);
+function Phone() {
+    this.call = function(number) {
+        alert("Calling " + number + " via " + this.phoneNumber);
+    };
+    this.sendSms = function(message) {
+        alert("Sending message via " + this.phoneNumber + " : " + message);
+    };
+};
+
+function CellPhone(name, brand, phoneNumber, hasRadio) {
+    this.name = name;
+    this.brand = brand;
+    this.phoneNumber = phoneNumber;
+    this.hasRadio = hasRadio;
+    this.dialpadLayout = "en-US";
+
+    this.listenRadio = function() {
+        
+        if(this.hasRadio) {
+            alert("Listening to radio");
+        }
+        else {
+            alert("Sorry, your mobile doesn't support radio");
+        }
+    };
+};
+
+CellPhone.prototype = new Phone();
+
+function SmartPhone() {
+    this.installApp = function(appName) {
+        alert("Installing " + appName + " on " + this.name);
+    };
+    this.takePicture = function(){
+        if(this.camSensorSize > 7){
+            alert("Taking picture..")
+        }
+        else{
+            alert("Camera quality is bad..")
+        }
     }
 };
- 
-const AndroidPhone = function(name, brand) {
+
+SmartPhone.prototype = new Phone();
+
+function AndroidPhone (name, brand, phoneNumber, display, camSensorSize, osVersion, hasNFC) {
+    this.name=name;
+    this.brand = brand;
+    this.phoneNumber = phoneNumber;
+    this.display = display;
+    this.camSensorSize = camSensorSize;
+    this.osVersion = osVersion;  
+    this.hasNFC = hasNFC;  
+
+    
+    this.useNFC = function() {
+        if (this.hasNFC) {
+            alert("Using NFC feature");
+        } else {
+            alert("NFC not supported on this device");
+        }
+    };
+};
+
+AndroidPhone.prototype = new SmartPhone();
+
+function iOSPhone(name, brand, phoneNumber, display, camSensorSize, iosVersion) {
     this.name = name;
     this.brand = brand;
-    this.os = "android";
+    this.phoneNumber = phoneNumber;
+    this.display = display;
+    this.camSensorSize = camSensorSize;
+    this.iosVersion = iosVersion;  
+
+    this.useSiri = function() {
+        alert("Using Siri on " + this.name);
+    };
 };
- 
-AndroidPhone.prototype = Phone;
 
-
-const IOSPhone = function(name, brand) {
-    this.name = name;
-    this.brand = brand;
-    this.os = "android";
-};
- 
-IOSPhone.prototype = Phone;
-
-
-const redmi = new AndroidPhone("Redmi Note 9", "Redmi");
-const input = document.getElementById("input-box");
-function call(){
-    if(input.value.length<1){
-        alert("Invalid input");
-        return;
-    }    
-    redmi.call(input.value);
-}
-
-function message(){
-    if(input.value.length<1){
-        alert("Invalid input");
-        return;
-    }    
-    redmi.sendMessage(input.value)
-}
-
-function browse(){
-    if(input.value.length<1){
-        alert("Invalid input");
-        return;
-    }    
-    redmi.browse(input.value)
-}
+iOSPhone.prototype = new SmartPhone();
